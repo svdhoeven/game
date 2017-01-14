97dbf2c40152f6c55ba8ad1f55e73343edbca398
Sven <svdhoeven@twccgaming.com>
2017-01-14 23:06:34 +0100

    Gameobject

---
+++ src/js/gameObjects/GameObject.js
@@ -0,0 +1,19 @@
+class GameObject{
+    //Fields
+    x = 0;
+    y = 0;
+    z = 0;
+
+    /**
+     * Constructor
+     *
+     * @param x
+     * @param y
+     * @param z
+     */
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
